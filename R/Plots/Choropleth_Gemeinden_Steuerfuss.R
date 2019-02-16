79850e04296f099fd452f82043431a2699cd20d1
FlyingTom63 <47270380+FlyingTom63@users.noreply.github.com>
2019-02-16 10:19:31 +0100

    Ubernommen und lauffähig gemacht

---
+++ R/Plots/Choropleth_Gemeinden_Steuerfuss.R
@@ -0,0 +1,77 @@
+library(rgeos)
+library(maptools)
+library(rgdal)
+library(ggplot2)
+library(plyr)
+
+# READ IN DATA
+gd_dat <- read.csv("data/output/BDA6_SCHULE_Gemeinden_Plot_Steuern.csv")
+
+# WHAT COLUMNS DO WE HAVE?
+colwise(class)(gd_dat)
+
+rgdal::ogrInfo("data/shapes/Gemeinden/GEMKTLU0_PY.shp")
+
+gd_map <- rgdal::readOGR("data/shapes/Gemeinden/GEMKTLU0_PY.shp")
+plot(gd_map)
+
+gd_map <- fortify(gd_map, region = "BFS_NR")
+
+gd_map_centers <- ddply(gd_map, .(id), summarize, clat = mean(lat), clong = mean(long))
+
+ggplot() +
+  labs(
+    title="Steuerbelastung nach Gemeinden gemäss Steuerprozenten",
+    subtitle="5 Klassen, gleichverteilt, keine Missing Values",
+    x=NULL,
+    y=NULL) +
+  geom_map (
+    data = gd_dat,
+    aes(map_id = BFS_NR, fill = Klasse, color = "red"),
+    map = gd_map) + 
+  expand_limits (
+    x = gd_map$long,
+    y = gd_map$lat) + 
+  geom_text (data = gd_map_centers, aes(x = clong, y = clat, label = id))
+  
+ggplot() +
+  labs(
+     title="Steuerbelastung nach Gemeinden gemäss ",
+     subtitle="Analyse der Rohdaten (Werte 1-1000, Missing Values -1000)",
+     x=NULL,
+     y=NULL) +
+  geom_map (
+     data = gd_dat,
+     aes(map_id = BFS_NR, fill = Klasse),
+     map = gd_map) + 
+  expand_limits (
+     x = gd_map$long,
+     y = gd_map$lat) + 
+  geom_text (data = gd_map_centers, aes(x = clong, y = clat, label = id)) +
+  scale_fill_gradient2 (
+     limits=c(-1000,1000),
+     low="red",
+     mid="white", midpoint=0,
+     high="blue")
+
+
+
+ggplot() + ggtitle(label) +
+  geom_map (
+    data = gd_dat,
+    aes(map_id = BFS_NR, fill = MY_PRCT),
+    map = gd_map) + 
+  expand_limits (
+    x = gd_map$long,
+    y = gd_map$lat) + 
+  geom_text (data = gd_map_centers, aes(x = clong, y = clat, label = id)) +
+  scale_colour_discrete(name  ="Klasse",
+                        breaks=c("3", "4"),
+                        labels=c("3", "4")) +
+  scale_shape_discrete(name  ="Klasse",
+                       breaks=c("3", "4"),
+                       labels=c("3", "4"))
+
+
+#scale_fill_gradient2(limits=c(-20,60), low=muted("red"), mid="white", 
+#                     midpoint=0, high=muted("blue"))
